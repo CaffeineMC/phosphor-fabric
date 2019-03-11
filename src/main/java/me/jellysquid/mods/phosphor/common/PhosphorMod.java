e473a14752e1b85497a401b1a4a5068888368b6e
Angeline <jellysquid3@users.noreply.github.com>
2019-03-10 19:21:28 -0500

    Bump version to 0.1.3

--- src/main/java/me/jellysquid/mods/phosphor/common/PhosphorMod.java
+++ src/main/java/me/jellysquid/mods/phosphor/common/PhosphorMod.java
@@ -15,7 +15,7 @@ public class PhosphorMod {
 
     public static final String MOD_NAME = "Phosphor";
 
-    public static final String MOD_VERSION = "1.12.2-0.1.2";
+    public static final String MOD_VERSION = "1.12.2-0.1.3";
 
     public static final String MOD_FINGERPRINT = "f0387d288626cc2d937daa504e74af570c52a2f1";
 
