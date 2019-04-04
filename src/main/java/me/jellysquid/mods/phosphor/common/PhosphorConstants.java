e27f3ae0e7c7efe04e6e14add2165c0b1f47c680
Angeline <jellysquid3@users.noreply.github.com>
2019-04-04 03:03:41 -0500

    Bump version to 0.2.1

--- src/main/java/me/jellysquid/mods/phosphor/common/PhosphorConstants.java
+++ src/main/java/me/jellysquid/mods/phosphor/common/PhosphorConstants.java
@@ -5,7 +5,7 @@ public final class PhosphorConstants {
 
     public static final String MOD_NAME = "Phosphor";
 
-    public static final String MOD_VERSION = "1.12.2-0.2.0";
+    public static final String MOD_VERSION = "1.12.2-0.2.1";
 
     public static final String MOD_FINGERPRINT = "f0387d288626cc2d937daa504e74af570c52a2f1";
 }
