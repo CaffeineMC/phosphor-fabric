aceb89d86ba08eea83204997a0d363f5ef946d06
Angeline <jellysquid3@users.noreply.github.com>
2019-04-12 21:42:05 -0500

    Reduce network traffic a bit and only send chunks when the lighting inside of them is non-trivial

--- src/main/java/me/jellysquid/mods/phosphor/mixins/lighting/common/MixinExtendedBlockStorage.java
+++ src/main/java/me/jellysquid/mods/phosphor/mixins/lighting/common/MixinExtendedBlockStorage.java
@@ -1,18 +1,95 @@
 package me.jellysquid.mods.phosphor.mixins.lighting.common;
 
+import net.minecraft.world.chunk.NibbleArray;
 import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ExtendedBlockStorage.class)
 public class MixinExtendedBlockStorage {
+    @Shadow
+    private NibbleArray skyLight;
+
+    @Shadow
+    private int blockRefCount;
+
+    @Shadow
+    private NibbleArray blockLight;
+
+    private int lightRefCount = -1;
+
     /**
      * @author Angeline
-     * @reason Always send chunks to the client so non-trivial lighting is accounted for. This incurs a small increase
-     * in network traffic, but Minecraft compresses most of that away.
+     * @author Reset lightRefCount on call
+     */
+    @Overwrite
+    public void setSkyLight(int x, int y, int z, int value) {
+        this.skyLight.set(x, y, z, value);
+        this.lightRefCount = -1;
+    }
+
+    /**
+     * @author Angeline
+     * @author Reset lightRefCount on call
+     */
+    @Overwrite
+    public void setBlockLight(int x, int y, int z, int value) {
+        this.blockLight.set(x, y, z, value);
+        this.lightRefCount = -1;
+    }
+
+    /**
+     * @author Angeline
+     * @author Reset lightRefCount on call
+     */
+    @Overwrite
+    public void setBlockLight(NibbleArray array) {
+        this.blockLight = array;
+        this.lightRefCount = -1;
+    }
+
+    /**
+     * @author Angeline
+     * @reason Reset lightRefCount on call
+     */
+    @Overwrite
+    public void setSkyLight(NibbleArray array) {
+        this.skyLight = array;
+        this.lightRefCount = -1;
+    }
+
+
+    /**
+     * @author Angeline
+     * @reason Send light data to clients when lighting is non-trivial
      */
     @Overwrite
     public boolean isEmpty() {
+        if (this.blockRefCount != 0) {
+            return false;
+        }
+
+        // -1 indicates the lightRefCount needs to be re-calculated
+        if (this.lightRefCount == -1) {
+            if (this.checkLightArrayEqual(this.skyLight.getData(), (byte) 0xFF)
+                    && this.checkLightArrayEqual(this.blockLight.getData(), (byte) 0x00)) {
+                this.lightRefCount = 0; // Lighting is trivial, don't send to clients
+            } else {
+                this.lightRefCount = 1; // Lighting is not trivial, send to clients
+            }
+        }
+
+        return this.lightRefCount == 0;
+    }
+
+    private boolean checkLightArrayEqual(byte[] arr, byte val) {
+        for (byte b : arr) {
+            if (b != val) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
