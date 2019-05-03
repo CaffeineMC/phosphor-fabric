78d87667ffb48a7d920345e6a9ce12ee65cc0957
Angeline <jellysquid3@users.noreply.github.com>
2019-05-03 15:54:28 -0500

    Bump version to 0.2.3

--- src/main/java/me/jellysquid/mods/phosphor/mod/PhosphorConstants.java
+++ src/main/java/me/jellysquid/mods/phosphor/mod/PhosphorConstants.java
@@ -1,13 +1,13 @@
 package me.jellysquid.mods.phosphor.mod;
 
-public final class PhosphorConstants {
-    public static final String MOD_ID = "phosphor-lighting";
+final class PhosphorConstants {
+    static final String MOD_ID = "phosphor-lighting";
 
-    public static final String MOD_NAME = "Phosphor";
+    static final String MOD_NAME = "Phosphor";
 
-    public static final String MOD_VERSION = "1.12.2-0.2.2";
+    static final String MOD_VERSION = "1.12.2-0.2.3";
 
-    public static final String MOD_FINGERPRINT = "f0387d288626cc2d937daa504e74af570c52a2f1";
+    static final String MOD_FINGERPRINT = "f0387d288626cc2d937daa504e74af570c52a2f1";
 
-    public static final String MOD_DEPENDENCIES = "after:neid@[1.5.4.4,);after:spongeforge@[1.12.2-2768-7.1.6-RC3639,)";
+    static final String MOD_DEPENDENCIES = "after:neid@[1.5.4.4,);after:spongeforge@[1.12.2-2768-7.1.6-RC3639,)";
 }
