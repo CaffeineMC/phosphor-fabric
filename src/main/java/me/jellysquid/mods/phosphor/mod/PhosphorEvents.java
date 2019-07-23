d8e94737a422a234f795dd58381ed92e07330c35
Angeline <jellysquid3@users.noreply.github.com>
2019-07-23 17:53:47 -0500

    Add a special first-join message for Aether II players

--- src/main/java/me/jellysquid/mods/phosphor/mod/PhosphorEvents.java
+++ src/main/java/me/jellysquid/mods/phosphor/mod/PhosphorEvents.java
@@ -1,10 +1,15 @@
 package me.jellysquid.mods.phosphor.mod;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.event.ClickEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.fml.common.CertificateHelper;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
@@ -28,12 +33,32 @@ public class PhosphorEvents {
             return;
         }
 
-        player.sendMessage(new TextComponentString(TextFormatting.GOLD.toString() +
-                "Thanks for installing Phosphor! ❤ You can pledge a cup of coffee to me every month " +
-                "so you can look forward to future mods like this one. " +
-                TextFormatting.YELLOW.toString() + TextFormatting.UNDERLINE.toString() + "https://patreon.com/jellysquid"));
+        if (PhosphorEvents.isAetherInstalled()) {
+            player.sendMessage(new TextComponentString(TextFormatting.YELLOW + "The Aether II includes the Phosphor mod! ❤ " + TextFormatting.GOLD +
+                    "You can help fund the development of Phosphor with a pledge to the author's Patreon. " + TextFormatting.YELLOW + TextFormatting.UNDERLINE + "Click this link to make a pledge!")
+                    .setStyle(new Style().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.patreon.com/jellysquid"))));
+        } else {
+            player.sendMessage(new TextComponentString(TextFormatting.YELLOW + "Thanks for installing Phosphor! ❤ " + TextFormatting.GOLD + "You can help fund the development of " +
+                    "future optimization mods like this one through pledging to my Patreon. " + TextFormatting.YELLOW + TextFormatting.UNDERLINE + "Click this link to make a pledge!")
+                    .setStyle(new Style().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.patreon.com/jellysquid"))));
+        }
+
 
         config.showPatreonMessage = false;
         config.saveConfig();
+    }
+
+    private static boolean isAetherInstalled() {
+        if (!Loader.isModLoaded("aether")) {
+            return false;
+        }
+
+        ModContainer mod = Loader.instance().getIndexedModList().get("aether");
+
+        if (mod == null) {
+            return false;
+        }
+
+        return CertificateHelper.getFingerprint(mod.getSigningCertificate()).equals("db341c083b1b8ce9160a769b569ef6737b3f4cdf");
     }
 }
