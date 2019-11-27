58a084b06c790fab9235695166d362267367ff5d
Angeline <jellysquid3@users.noreply.github.com>
2019-11-27 14:53:48 -0600

    Initialize the correct logging framework, fixes dedicated server crash

--- src/main/java/me/jellysquid/mods/phosphor/common/PhosphorMod.java
+++ src/main/java/me/jellysquid/mods/phosphor/common/PhosphorMod.java
@@ -1,14 +1,15 @@
 package me.jellysquid.mods.phosphor.common;
 
 import net.fabricmc.api.ModInitializer;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SuppressWarnings("unused")
 public class PhosphorMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        Log log = LogFactory.getLog("Phosphor");
-        log.warn("This is an early-access version of Phosphor which could cause issues in your game. Please be mindful.");
+        Logger log = LogManager.getLogger("Phosphor");
+        log.warn("Phosphor has been initialized. Please note that you are using an early release of Phosphor which " +
+                "*could* result in unexpected issues or crashes. You can join the Discord server to get support here: https://discord.gg/X2TuKe");
     }
 }
