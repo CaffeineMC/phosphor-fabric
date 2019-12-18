1fb521b9209d6dafef64a39dcd1421245df5a86d
Angeline <jellysquid3@users.noreply.github.com>
2019-12-18 16:41:19 -0600

    change: Remove startup warning

--- src/main/java/me/jellysquid/mods/phosphor/common/PhosphorMod.java
+++ src/main/java/me/jellysquid/mods/phosphor/common/PhosphorMod.java
@@ -1,15 +1,9 @@
 package me.jellysquid.mods.phosphor.common;
 
 import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @SuppressWarnings("unused")
 public class PhosphorMod implements ModInitializer {
     @Override
-    public void onInitialize() {
-        Logger log = LogManager.getLogger("Phosphor");
-        log.warn("Phosphor has been initialized. Please note that you are using an early release of Phosphor which " +
-                "*could* result in unexpected issues or crashes. You can join the Discord server to get support here: https://discord.gg/X2TuKe");
-    }
+    public void onInitialize() { }
 }
