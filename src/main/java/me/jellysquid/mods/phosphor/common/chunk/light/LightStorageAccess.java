6c5b89d2a8a82e7b645bc200efc1b868d197c123
Philipp Provenzano <PProvenzano@web.de>
2020-08-29 19:24:41 +0200

    new: Implement cleanups discussed in MC-196542 (#16)
    * Implement cleanups discussed in MC-196542
    
    * Reduce map lookups in the skylight optimization code
    
    * Small style cleanups

---
+++ src/main/java/me/jellysquid/mods/phosphor/common/chunk/light/LightStorageAccess.java
@@ -0,0 +1,13 @@
+package me.jellysquid.mods.phosphor.common.chunk.light;
+
+import net.minecraft.world.chunk.ChunkNibbleArray;
+
+public interface LightStorageAccess {
+    ChunkNibbleArray callGetLightArray(long sectionPos, boolean cached);
+
+    /**
+     * Returns the light value for a position that does not have an associated lightmap.
+     * This is analogous to {@link net.minecraft.world.chunk.light.LightStorage#getLight(long)}, but uses the cached light data.
+     */
+    int getLightWithoutLightmap(long blockPos);
+}
