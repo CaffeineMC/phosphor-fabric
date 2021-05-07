a26c6c520963b6c3fccb01937fe77ca287beb547
RyuuM3gum1n <maurermarkuss6@gmail.com>
2021-05-07 22:11:09 +0200

    Update to 1.16.5

--- src/main/java/me/jellysquid/mods/phosphor/mixin/chunk/light/MixinChunkLightProvider.java
+++ src/main/java/me/jellysquid/mods/phosphor/mixin/chunk/light/MixinChunkLightProvider.java
@@ -74,7 +74,7 @@ public abstract class MixinChunkLightProvider<M extends ChunkToNibbleArrayMap<M>
     // [VanillaCopy] method_20479
     @Override
     public BlockState getBlockStateForLighting(int x, int y, int z) {
-        if (World.isHeightInvalid(y)) {
+        if (World.isOutOfBuildLimitVertically(y)) {
             return DEFAULT_STATE;
         }
 
